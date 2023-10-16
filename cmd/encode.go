a823dea0cc035e059b6b10e1faf7659d9220364f
Rohan Yadav <rajrohanyadav@gmail.com>
2023-10-16 11:22:37 +0530

    fix linting

--- cmd/encode.go
+++ cmd/encode.go
@@ -19,8 +19,8 @@ import (
 	b64 "encoding/base64"
 	"fmt"
 
-	"github.com/spf13/cobra"
 	"github.com/rajrohanyadav/dtx/cmd/utils"
+	"github.com/spf13/cobra"
 )
 
 // encodeCmd represents the encode command
